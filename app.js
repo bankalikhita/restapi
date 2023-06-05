3c9c9e9f6a8d7cba12dd155cb84e65022e29943a
bankalikhita <likhita199777@gmail.com>
2023-06-05 09:40:22 +0000

    Authentication

--- app.js
+++ app.js
@@ -30,18 +30,67 @@ app.post("/register", async (request, response) => {
   const checkuserquery = `select * from user WHERE username='${username}';`;
   let dbuser = await db.get(checkuserquery);
   if (dbuser === undefined) {
-    let createuserquery = `insert into user (username,name,password,gender,location) values('$(username)','$(name)','$(hashedPassword)','$(gender)','$(location)');`;
+    let createuserquery = `insert into user (username,name,password,gender,location) values('${username}','${name}','${hashedPassword}','${gender}','${location}');`;
     if (password.length < 5) {
-      response.send("Password is too short");
       response.status(400);
+      response.send("Password is too short");
     } else {
       await db.run(createuserquery);
-      response.send("User created succesfully");
       response.status(200);
+      response.send("User created successfully");
     }
   } else {
-    response.send("User already exists");
     response.status(400);
+    response.send("User already exists");
+  }
+});
+
+app.post("/login", async (request, response) => {
+  const logindetails = request.body;
+  const { username, password } = logindetails;
+  const checkuser = `select * from user WHERE username='${username}';`;
+  let dbuser = await db.get(checkuser);
+  if (dbuser === undefined) {
+    response.status(400);
+    response.send("Invalid user");
+  } else {
+    const pwdmatch = await bcrypt.compare(password, dbuser.password);
+    if (pwdmatch === true) {
+      response.send("Login success!");
+      response.status(400);
+    } else {
+      response.status(400);
+      response.send("Invalid password");
+    }
+  }
+});
+
+app.put("/change-password", async (request, response) => {
+  const changepwd = request.body;
+  const { username, oldPassword, newPassword } = changepwd;
+  const getuser = `select * from user where username='${username}';`;
+  const userdet = await db.get(getuser);
+
+  if (userdet === undefined) {
+    response.status(400);
+    response.send("User not registered");
+  } else {
+    const pwdmatch = await bcrypt.compare(oldPassword, userdet.password);
+    if (pwdmatch === false) {
+      response.status(400);
+      response.send("Invalid current password");
+    } else {
+      if (newPassword.length < 5) {
+        response.status(400);
+        response.send("Password is too short");
+      } else {
+        let newhashedPassword = await bcrypt.hash(newPassword, 10);
+        const updatepwd = `update user set password='${newhashedPassword}' where username='${username}';`;
+        await db.run(updatepwd);
+        response.send("Password updated");
+        response.status(200);
+      }
+    }
   }
 });
 module.exports = app;
